0bf3632b22acfc7b6432e79d5ffa4bc58c8aacfb
Vahric <vahric@gmail.com>
2023-03-21 01:22:09 +0300

    api1 ve 2

---
+++ firstflaskfiles/flaskapi.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/api1/', methods=['GET', 'POST'])
+def fonapi1():
+    return "Api1 cevap veriyor"
+
+@app.route('/api2/', methods=['GET', 'POST'])
+def fonapi2():
+    return "Api2 cevap veriyor"
+
+if __name__ == '__main__':
+    app.run()
